7483a2df7032ee2072cde9a12b4278681a50a05b
maryapple <maria9ku@gmail.com>
2017-12-24 01:50:53 +0300

    task 6

--- final.cpp
+++ final.cpp
@@ -1,78 +1,88 @@
 #include <iostream>
-#define n 6
-#define m 4
+#define N 3
+#define M 4
 
 using namespace std;
 
-void input(int A[n][m]);
-void output2(int A[n][m]);
-void make_array(int A[n][m], int B[m]);
-void output1(int B[m]);
-void sort_lines(int A[n][m]);
+void input(int ** A, int *n);
+void output_array2(int ** A);
+void output(int * A, int negative_odd_cnt, int negative_even_cnt, int n);
+void make_array(int ** A, int * B);
+void get_answer(int * A, int n, int *negative_odd_cnt, int *negative_even_cnt);
 
-int main(){
-	int A[n][m], B[m];
-	input(A);
-	cout << "\nInitial array: \n";
-	output2(A);
+int main() {
+	int n = 0, negative_odd_cnt = 0, negative_even_cnt = 0;
+	int ** A = new int *[N];
+	for (int i = 0; i < N; i++)
+		A[i] = new int[M];
+	int * B = new int[N * M];
+	input(A, &n);
 	make_array(A, B);
-	cout << "\nArray of indexes of minimal elements in columns: \n\t";
-	output1(B);
-	sort_lines(A);
-	cout << "\n\nSorted array:\n";
-	output2(A);
+	get_answer(B, n, &negative_odd_cnt, &negative_even_cnt);
+	output_array2(A);
+	output(B, negative_odd_cnt, negative_even_cnt, n);
+
 	return 0;
 }
 
-void input(int A[n][m]){
-	int i=0, j=0;
-	for (i=0; i<n; i++){
-		for (j=0; j<m; j++){
-			cout << "Enter [" << i << "][" << j << "] element: ";
+void input(int ** A, int *n)
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cout << "Enter A[" << i << "][" << j << "]: ";
 			cin >> A[i][j];
 		}
 	}
+	cout << "Enter n: ";
+	cin >> *n;
 }
 
-void output2(int A[n][m]){
-	int i=0, j=0;
-	for (i=0; i<n; i++){
-		for (j=0; j<m; j++) cout << "\t" << A[i][j];
-		cout << endl;
+void output_array2(int ** A)
+{
+	cout << "2-dimensions array:\n";
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+			cout << "\t" << A[i][j];
+		cout << "\n";
 	}
 }
 
-void make_array(int A[n][m], int B[m]){
-	int i=0, j=0, min=0, index=0;
-	for (j=0; j<m; j++){
-		min=A[0][j]; 
-		index=0;
-		for (i=0; i<n; i++){
-			if (A[i][j] < min){ 
-				min=A[i][j];
-				index=i;
-			}
-		}
-		B[j]=index;
-	}
+void output(int * A, int negative_odd_cnt, int negative_even_cnt, int n)
+{
+	cout << "1-dimension array:\n";
+	for (int i = 0; i < N * M; i++)
+		cout << A[i] << " ";
+	cout << "\n";
+	if (negative_even_cnt + negative_odd_cnt < n)
+		cout << "There are less than " << n << " negative numbers in the array!\n";
+	else
+		cout << "There are " << negative_even_cnt << " even negative numbers and " << negative_odd_cnt << " odd negative numbers among first " << n << " negative elements in the array.\n";
 }
 
-void output1(int B[m]){
-	int j=0;
-	for (j=0; j<m; j++) cout << B[j] << "\t";
+void make_array(int ** A, int * B)
+{
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			B[i * M + j] = A[i][j];
 }
 
-void sort_lines(int A[n][m]){
-	int i=0, j=0, buf=0, k=0;
-	for (i=0; i<n; i++){
-		for (k=0; k<m-1; k++){
-			for (j=0; j<m-1; j++){
-				if (A[i][j] > A[i][j+1]){
-					buf=A[i][j];
-					A[i][j]=A[i][j+1];
-					A[i][j+1]=buf;
-				}
-			}
+void get_answer(int * A, int n, int *negative_odd_cnt, int *negative_even_cnt)
+{
+	*negative_odd_cnt = 0;
+	*negative_even_cnt = 0;
+	for (int i = 0; i < N * M; i++)
+	{
+		if ((*negative_even_cnt) + (*negative_odd_cnt) == n)
+			break;
+		if (A[i] < 0)
+		{
+			if ((-A[i]) % 2 == 0)
+				(*negative_even_cnt)++;
+			else
+				(*negative_odd_cnt)++;
 		}
 	}
 }
